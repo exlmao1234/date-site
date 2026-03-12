c6542530517d7c3c462d3f09c658c2ce279b999f
exlmao1234 <iancontreras510@gmail.com>
2026-03-12 07:34:30 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,17 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 export const metadata: Metadata = {
   title: "For Mamiringui 💗",
   description: "Papiringui made something special for you...",
@@ -23,7 +37,20 @@ export const metadata: Metadata = {
     description: "Papiringui made something special for you...",
     images: ["https://secretitojiji.vercel.app/preview.png"],
   },
-  icons: {
-    icon: "/favicon.ico",
-  },
 };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
